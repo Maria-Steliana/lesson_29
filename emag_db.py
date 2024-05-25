01d383e593ac8b4e745cf4c88ca9a05491488144
Maria-Steliana Cârlan <maya_dlr03@yahoo.com>
2024-05-25 16:37:22 +0300

    new function added for a new product; styling home.html

--- emag_db.py
+++ emag_db.py
@@ -18,7 +18,6 @@ def read_admins(config: dict, table: str = "emag.emag_admin"):
             return users
 
 
-
 def read_products(config: dict, table: str = "emag.products"):
     with ps.connect(**config) as conn:
         with conn.cursor() as cursor:
@@ -33,9 +32,23 @@ def read_products(config: dict, table: str = "emag.products"):
             return products_list
 
 
+def add_product(config: dict, product: dict, table: str = "emag.products"):
+    with ps.connect(**config) as conn:
+        with conn.cursor() as cursor:
+            sql_query = f"INSERT INTO {table} (name, store, price) VALUES (%s, %s, %s)"
+            cursor.execute(sql_query, (product['name'], product['store'], product['price']))
+            conn.commit() # confirma modificarile in baza de date
+
+
 if __name__ == '__main__':
     config = read_config()
     admins = read_admins(config)
-    products = read_products()
+
+    products = read_products(config)
+    new_prod = input("Enter all data about product: name/store/price -> ")
+    new_prod = new_prod.split("/")
+    new_prod = {"name": new_prod[0], "store": new_prod[1], "price": (new_prod[2])}
+    if new_prod['name'] not in [prod['name'] for prod in products]:
+        add_product(config, new_prod)
 
 
